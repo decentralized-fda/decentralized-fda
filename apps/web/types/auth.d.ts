5424a4258a7c771fc1e334dad2beabd3adbf8087
Mike P. Sinn <m@crowdsourcingcures.org>
2024-12-13 19:11:30 -0600

    Added types

---
+++ apps/web/types/auth.d.ts
@@ -0,0 +1,33 @@
+import { User as NextAuthUser } from "next-auth"
+import { JWT } from "next-auth/jwt"
+
+type UserId = string
+
+type NavUser = {
+  user?: NextAuthUser & {
+    id: UserId
+    username?: string
+    admin?: boolean
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: UserId
+  }
+}
+
+declare module "next-auth" {
+  interface Session {
+    user: NextAuthUser & {
+      id: UserId
+      username: string
+      admin?: boolean
+    }
+  }
+}
+
+export interface ExtendedUser extends NextAuthUser {
+  admin?: boolean
+  username?: string
+}
