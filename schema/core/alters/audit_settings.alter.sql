6b4bb1bb4ba8c956ea9be26023182d0a59bec79b
Mike P. Sinn <m@crowdsourcingcures.org>
2025-03-24 02:35:52 -0500

    Remove outdated audit-related SQL files to streamline schema and improve maintenance.

--- schema/core/alters/audit_settings.alter.sql
+++
@@ -1,4 +0,0 @@
--- Alter: core.audit_settings
--- Alter statements for core.audit_settings
-
-ALTER TABLE core.audit_settings ENABLE ROW LEVEL SECURITY;
--- schema/core/alters/audit_trail.alter.sql
+++
@@ -1,4 +0,0 @@
--- Alter: core.audit_trail
--- Alter statements for core.audit_trail
-
-ALTER TABLE core.audit_trail ENABLE ROW LEVEL SECURITY;
