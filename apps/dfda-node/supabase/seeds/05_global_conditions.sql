c6ee7112a6969208d8563f6ba0e6b23849adc3d5
Mike P. Sinn <m@crowdsourcingcures.org>
2025-04-26 19:26:50 -0500

    Add new boolean unit and update health conditions: Introduce 'boolean' unit for health metrics in 03_units.sql. Update 04_global_variables.sql to reflect new unit associations for various health conditions. Add common health conditions with corresponding codes in 05_global_conditions.sql, enhancing the dataset for health tracking.

--- apps/dfda-node/supabase/seeds/05_global_conditions.sql
+++ apps/dfda-node/supabase/seeds/05_global_conditions.sql
@@ -11,4 +11,27 @@ VALUES
 ('major-depressive-disorder', 'F33'),
 ('asthma', 'J45'),
 ('headache', 'R51'),
-('pain', 'R52');
+('pain', 'R52'),
+-- Added Common Conditions Start
+('hyperlipidemia', 'E78.5'),
+('mixed-hyperlipidemia', 'E78.2'),
+('low-back-pain', 'M54.5'),
+('copd', 'J44.9'),
+('atrial-fibrillation', 'I48.91'),
+('abdominal-pain', 'R10.9'),
+('uti', 'N39.0'),
+('anxiety', 'F41.9'),
+('gerd', 'K21.9'),
+('chest-pain', 'R07.9'),
+('upper-respiratory-infection', 'J06.9'),
+('pneumonia', 'J18.9'),
+('acute-bronchitis', 'J20.9'),
+('knee-pain', 'M25.569'),
+('heart-failure', 'I50.9'),
+('hypothyroidism', 'E03.9'),
+('vitamin-d-deficiency', 'E55.9'),
+-- Added Common Conditions End
+-- Added Long COVID
+('long-covid', 'U09.9'),
+-- Added COVID-19
+('covid-19', 'U07.1');
