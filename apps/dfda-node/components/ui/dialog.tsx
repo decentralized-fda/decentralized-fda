2972a20848e11a388f2dd6f6a13af34dfa36075d
Mike P. Sinn <m@crowdsourcingcures.org>
2026-08-24 14:45:28 -0500

    fix(dfda-node): bridge mixed React child types

--- apps/dfda-node/components/ui/dialog.tsx
+++ apps/dfda-node/components/ui/dialog.tsx
@@ -8,7 +8,20 @@ import { cn } from "@/lib/utils"
 
 const Dialog = DialogPrimitive.Root
 
-const DialogTrigger = DialogPrimitive.Trigger
+type DialogPrimitiveTriggerProps = React.ComponentPropsWithoutRef<typeof DialogPrimitive.Trigger>
+type DialogTriggerProps = Omit<DialogPrimitiveTriggerProps, "children"> & {
+  children?: React.ReactNode
+}
+
+const DialogTrigger = React.forwardRef<
+  React.ElementRef<typeof DialogPrimitive.Trigger>,
+  DialogTriggerProps
+>(({ children, ...props }, ref) => (
+  <DialogPrimitive.Trigger ref={ref} {...props}>
+    {children as DialogPrimitiveTriggerProps["children"]}
+  </DialogPrimitive.Trigger>
+))
+DialogTrigger.displayName = DialogPrimitive.Trigger.displayName
 
 const DialogPortal = DialogPrimitive.Portal
 
